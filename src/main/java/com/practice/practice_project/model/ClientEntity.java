2a9fe41b92efafffe7c21fb153bb9a2bb6a51bc5
KozhomyrzaAkylbekov <akylbekov04@inbox.ru>
2024-12-08 04:06:34 +0600

    initial

---
+++ src/main/java/com/practice/practice_project/model/ClientEntity.java
@@ -0,0 +1,25 @@
+package com.practice.practice_project.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+@Entity
+@Table(name = "client_dbt")
+public class ClientEntity {
+    @Id
+    private Long id;
+    @Column(name = "full_name")
+    private String fullName;
+    @Column(name = "phone_number")
+    private String phoneNumber;
+    @OneToMany(mappedBy = "client", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<MovieEntity> movies;
+}
---
+++ src/main/java/com/practice/practice_project/service/db/MovieDbService.java
@@ -0,0 +1,37 @@
+package com.practice.practice_project.service.db;
+
+import com.practice.practice_project.common.enums.SystemCodes;
+import com.practice.practice_project.common.exceptions.BadRequestException;
+import com.practice.practice_project.dto.request.MovieRequest;
+import com.practice.practice_project.model.ClientEntity;
+import com.practice.practice_project.model.MovieEntity;
+import com.practice.practice_project.model.MovieRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class MovieDbService {
+    private final MovieRepository movieRepository;
+
+    public MovieEntity findByName(String name) {
+        return movieRepository.findByName(name)
+                .orElseThrow(() -> new BadRequestException(SystemCodes.ENTITY_NOT_FOUND));
+    }
+
+    public void saveEntity(MovieRequest request, ClientEntity client) {
+        movieRepository.save(MovieEntity.builder()
+                .name(request.getName())
+                .category(request.getCategory())
+                .rate(request.getRate())
+                .client(client)
+                .build());
+    }
+
+    public List<MovieEntity> findMoviesByKeywordOrderByCreationDate(String keyword) {
+        return movieRepository.findByNameContainingIgnoreCaseOrderByCreationDateDesc(keyword);
+    }
+}
